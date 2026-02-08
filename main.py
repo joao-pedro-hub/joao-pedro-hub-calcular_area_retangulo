da1595c66613a8b2d90a00588a29fc31841d026b
João Pedro Melo Soares <joaopedromelo1909ez@gmail.com>
2026-02-08 14:01:33 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#objetivo:
+#criar um programa que peça a altura e largura de um retangulo
+#calcular a area com os dados recebidos
+#mostrar o resutado
+
+print('Area de um retangulo:')
+
+altura=float(input('digite a altura do retangulo: '))
+
+largura=float(input('digite a largura do retangulo: '))
+
+area=altura*largura
+
+print(f'A area do retangulo é iqual a {area}')
